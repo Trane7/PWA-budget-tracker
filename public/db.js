77e0ce57aebb6cf592026e7ae731b0214ab06d8f
Dee <dgrowette@gmail.com>
2022-02-23 11:01:20 -0800

    finished saveRecord function and added deletePending function

--- public/db.js
+++ public/db.js
@@ -18,10 +18,44 @@ request.onerror = function (event) {
   console.log("Woops! " + event.target.errorCode);
 };
 
-function saveRecord(record) {
+function checkDatabase() {
   const transaction = db.transaction(["pending"], "readwrite");
   const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
 
-  store.add(record);
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
 }
 
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+  
+    store.add(record);
+}
+  
+
+function deletePending() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.clear();
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
