dd55575aa05fb0f657a541166eebf5042839993f
Dee <dgrowette@gmail.com>
2022-02-23 12:10:56 -0800

    added the install eventlistener in service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,3 +4,15 @@ const FILES_TO_CACHE = [
 
 const DATA_CACHE_NAME = "data-cache-v1"
 const CACHE_NAME = "stactic-cache-v2"
+
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully");
+            return cache.addAll(FI)
+        })
+    )
+
+    self.skipWaiting()
+})
+
